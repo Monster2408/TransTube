536df354a05e84c3dc2513a8813adcaa76633d1e
Monster2408 <meoto2408@gmail.com>
2024-06-27 05:17:14 +0900

    変換後に次のファイルを選択すると正しく更新されない問題と変換元ファイルを記憶する機能を追加

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 | 変換機能一覧 |
 |----|
 | MOV → MP4 |
+
+## EXE化
+```bat
+pyinstaller transtube.spec --clean
+```
--- transtube.py
+++ transtube.py
@@ -13,8 +13,19 @@ def resourcePath(filename):
         return os.path.join(sys._MEIPASS, filename)
     return os.path.join(filename)
 
+def get_user_dir():
+    iDir = os.path.abspath(os.path.dirname(__file__))
+    user_dir = os.path.join(iDir, 'user')
+    if not os.path.exists(user_dir):
+        os.makedirs(user_dir)
+    return user_dir
+
+def get_cache_file():
+    user_cache_file = os.path.join(get_user_dir(), 'cache.txt')
+    return user_cache_file
+
 EXE_NAME = 'TransTube'
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 
 SELECT_MOV2MP4 = 'MOV to MP4'
 
@@ -39,16 +50,33 @@ if __name__ == '__main__':
     #ラベル
     Static1 = tkinter.Label(text=u'変換元ファイル：')
     Static1.grid(row=0,column=0)
+    
+    user_cache_file = get_cache_file()
+    file_path = ''
+    if os.path.exists(user_cache_file):
+        with open(user_cache_file, 'r') as f:
+            file_path = f.read()
+    if not os.path.exists(file_path):
+        file_path = ''
+    
     #エントリー
     EditBox = tkinter.Entry()
-    EditBox.insert(tkinter.END,"")
+    EditBox.insert(tkinter.END, file_path)
     EditBox.grid(row=0,column=1)
 
     def select_file(event):
         fTyp = [("MOVファイル","*.mov")]
-        iDir = os.path.abspath(os.path.dirname(__file__))
+        if os.path.exists(EditBox.get()):
+            iDir = os.path.dirname(EditBox.get())
+        else:
+            iDir = os.path.abspath(os.path.dirname(__file__))
         file = tkinter.filedialog.askopenfilename(filetypes = fTyp,initialdir = iDir)
+        EditBox.delete(0, tkinter.END)
         EditBox.insert(tkinter.END,file)
+        
+        user_cache_file = get_cache_file()
+        with open(user_cache_file, 'w') as f:
+            f.write(file)
 
     #ボタン
     SelectFileButton = tkinter.Button(text=u'選択')
